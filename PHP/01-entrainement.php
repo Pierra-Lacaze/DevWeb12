882d94e0335bdef04579396cebe6343665baf97d
Pierral <kaalah@gmail.com>
2023-10-04 14:03:26 +0900

    Debut du cours PHP

---
+++ PHP/01-entrainement.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>On va débuter en PHP</h1>
+    <?php 
+
+    // Hello, c'est le debut du cours PHP, on y va
+    echo "Je suis une instruction d'affichage";
+
+?>
+</body>
+</html>
